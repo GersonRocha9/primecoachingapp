787a519de34af787a4a393b3eb7e8df562cfefd2
Gerson Rocha <gersonrocha9@gmail.com>
2025-11-13 09:51:17 -0300

    initial commit

---
+++ app.config.ts
@@ -0,0 +1,31 @@
+import { ConfigContext, ExpoConfig } from 'expo/config'
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  'name': 'primecoachingapp',
+  'slug': 'primecoachingapp',
+  'version': '1.0.0',
+  'orientation': 'portrait',
+  'icon': './assets/icon.png',
+  'userInterfaceStyle': 'light',
+  'newArchEnabled': true,
+  'splash': {
+    'image': './assets/splash-icon.png',
+    'resizeMode': 'contain',
+    'backgroundColor': '#ffffff',
+  },
+  'ios': {
+    'supportsTablet': true,
+  },
+  'android': {
+    'adaptiveIcon': {
+      'foregroundImage': './assets/adaptive-icon.png',
+      'backgroundColor': '#ffffff',
+    },
+    'edgeToEdgeEnabled': true,
+    'predictiveBackGestureEnabled': false,
+  },
+  'web': {
+    'favicon': './assets/favicon.png',
+  },
+})
